5acf835e96ac1c1b89b4488b2d3be630df8fc942
sourav0789 <souravbosu3487@gmail.com>
2024-10-02 04:36:49 +0600

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-START_IMG = environ.get('START_IMG', 'https://ibb.co.com/d0cv8P7 https://ibb.co.com/0yLXpB5 https://ibb.co.com/Wpn15DJ https://ibb.co.com/8NLkhdD')
+START_IMG = environ.get('START_IMG', 'https://envs.sh/WqL.jpg https://envs.sh/Wq5.jpg https://envs.sh/WqK.jpg')
 FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
 PAYPICS = (environ.get('PAYPICS', 'https://graph.org/file/f4db1c3ad3d9e38b328e6.jpg')).split()
